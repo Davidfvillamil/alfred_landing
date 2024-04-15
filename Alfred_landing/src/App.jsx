486007fc5654469573bfb407b1bddfae0e6d026b
David Villamil <davidfvillamilm@gmail.com>
2024-04-15 15:58:48 -0500

    arreglo de margenes

--- Alfred_landing/src/App.jsx
+++ Alfred_landing/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './Components/Home/Home'
 import Whyalfred from './Components/whyalfred/Whyalfred'
 import Videosection from './Components/videosection/videosection'
 import Pricing from './Components/pricing/pricing'
+import Footer from './Components/footer/footer'
 
 function App() {
   
@@ -18,6 +19,7 @@ function App() {
       <Whyalfred></Whyalfred>
       <Videosection></Videosection>
       <Pricing></Pricing>
+      <Footer/>
     </>
   )
 }
---
+++ Alfred_landing/src/Components/footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './footer.css'
+
+function Footer() {
+  return (
+    <>
+        <section className='footer-main-section'>
+            <div className='footer-main-container'>
+
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Footer
