2ec8ae428667b24770bcbbae93d9f02d652c12e1
David Villamil <davidfvillamilm@gmail.com>
2024-04-16 11:30:46 -0500

    footer

--- Alfred_landing/src/Components/footer/footer.jsx
+++ Alfred_landing/src/Components/footer/footer.jsx
@@ -2,12 +2,34 @@ import React from 'react'
 
 import './footer.css'
 
+import { CgMail } from "react-icons/cg";
+import { FaPhone } from "react-icons/fa";
+
 function Footer() {
   return (
     <>
         <section className='footer-main-section'>
             <div className='footer-main-container'>
-
+              <div className='footer-logo'>
+                Alfred.
+              </div>
+              <div className='menu-footer'>
+                <div>Menu</div>
+                <div>Home</div>
+                <div>Why Alfred</div>
+                <div>contact</div>
+              </div>
+              <div className='contact-footer'>
+                <div>Contacto</div>
+                <div className='contact-footer-phone'>
+                  <FaPhone className='footer-phone-logo'/>
+                  Numero de telefono
+                </div>
+                <div className='contact-footer-mail'> 
+                  <CgMail className='footer-mail-logo'/>  
+                  Alfred@gmail.com
+                </div>
+              </div>
             </div>
         </section>
     </>
