c0b45d39b08fa4a71fdc584929189bd55235ddf2
David Villamil <davidfvillamilm@gmail.com>
2024-04-17 13:15:43 -0500

    mejoras al home

--- Alfred_landing/src/Components/Home/Home.jsx
+++ Alfred_landing/src/Components/Home/Home.jsx
@@ -10,8 +10,8 @@ function Home() {
         <section id='home'>
             <div className='home-main-container'>
                 <section className='home-section-left'>
-                    <h1>Practice Speaking for IELTS with AI</h1>
-                    <p> Alfred guides you through the intricacies of this exam, asking questions and providing instant feedback and suggestions on your answers, helping you expand your vocabulary, correct your grammar, and refine your pronunciation to achieve a high band score</p>
+                    <h1>Perfect Your IELTS Speaking with AI</h1>
+                    <p> Master IELTS Speaking with AI. Alfred, your dedicated AI companion, assists you in perfecting your IELTS speaking skills. Receive instant feedback and personalized suggestions on grammar, pronunciation, and vocabulary, guiding you towards achieving your desired band score with confidence and precision.</p>
                     <div>
                         <button>Get Started</button>
                         <button className='home-left-button-2'>More info</button>
