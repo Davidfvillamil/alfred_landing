b5dafe0c42e5bfeb0a5bdd001c39259ceaa67b8e
David Villamil <davidfvillamilm@gmail.com>
2024-04-17 15:43:04 -0500

    apartado de why alfred

--- Alfred_landing/src/Components/whyalfred/Whyalfred.jsx
+++ Alfred_landing/src/Components/whyalfred/Whyalfred.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import Why from '../../assets/why.svg'
+import Anytime_anywhere from '../../assets/Anytime_anywhere.svg'
+import Feedback from '../../assets/feedback.svg'
+import Pronuntiation from '../../assets/Pronuntiation.svg'
 
 /*------------------------Importaciónd e estilos---------------------------*/
 
@@ -11,30 +14,30 @@ function Whyalfred() {
         <div className='whyalfred-main-container'>
             <section className='whyalfred-main-text'>
                 <div>
-                    <h2>Why ALfred?</h2>
+                    <h2>Why Alfred?</h2>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quibusdam, quam accusamus non qui quia a veniam
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique quibusdam, quam accusamus non qui quia a veniamdaw
                     </p>
                 </div>
             </section>
             <section className='whyalfred-box-container'>
                 <div className='whyalfred-card'>
                     <div>
-                        <img src={Why} alt="" />
+                        <img src={Anytime_anywhere} alt="" className='whyalfred-img-1'/>
                     </div>
                     <h4>Practice any time, any where</h4>
                 </div>
 
                 <div className='whyalfred-card'>
                     <div>
-                        <img src={Why} alt="" />
+                        <img src={Feedback} alt="" className='whyalfred-img-2'/>
                     </div>
                     <h4>Get instant feedback</h4>
                 </div>
 
                 <div className='whyalfred-card'>
                     <div>
-                        <img src={Why} alt="" />
+                        <img src={Pronuntiation} alt="" />
                     </div>
                     <h4>Expand your vocabulary and Correct your grammar</h4>
                 </div>
